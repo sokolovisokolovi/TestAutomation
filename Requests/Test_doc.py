d52ab8ef8e9664871564a3c8b8b3174e91cb3ba7
Ilya Sokolov <101402402+sokolovisokolovi@users.noreply.github.com>
2023-07-10 10:31:03 +0300

    Update Test_doc.py

--- Requests/Test_doc.py
+++ Requests/Test_doc.py
@@ -1,1 +1,11 @@
+import requests
+from configuration import SERVICE_URL
+
+
+def test_request():
+    response = requests.get(url=SERVICE_URL)
+    received_posts = response.json()
+    print(received_posts)
+    assert response.status_code == 200
+    assert len(received_posts) == 6
 
